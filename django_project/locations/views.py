c08585ec7a6fbbbdfa73e8d658872e9413cd85ed
Andriy Uvarov <13uvarov@gmail.com>
2020-04-21 14:28:19 +0300

    Remove previous task files and update urls.py and views.py

--- django_project/locations/views.py
+++ django_project/locations/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
 
-def index(request, text=""):
-    if not text:
-        return HttpResponse("Hello, world. You're at the locations index.")
+def index(request):
+    return HttpResponse("Hello, world. You're at the locations index.")
+
+
+def index_second(request, text=""):
     return HttpResponse(text)

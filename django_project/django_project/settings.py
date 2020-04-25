3b8844c3dfbc2e08af8ee01bc7e258bed02dbb71
Andriy Uvarov <13uvarov@gmail.com>
2020-04-25 23:21:50 +0300

    Task_8 initialization

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_django',
+        'USER': 'user_123',
+        'PASSWORD': 'pass_123',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
--- django_project/locations/models.py
+++ django_project/locations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Symbol(models.Model):
+    image = models.ImageField(null=False, blank=False)
+
+
+class Country(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100)
+    description = models.TextField(null=True)
+    population = models.IntegerField(default=0)
+    flag = models.OneToOneField(Symbol, on_delete=models.CASCADE)
+    cities_count = models.IntegerField(default=0)
+    users = models.ManyToManyField
+
+
+class City(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    longitude = models.FloatField(null=False)
+    latitude = models.FloatField(null=False)
+
+
+# Добавить в модель Country поле users со связью ManyToMany к стандартной модели пользователя. Создать миграции и провести их в базу.
